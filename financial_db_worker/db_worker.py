4fd262a4dcdb4f0e8b6a23b5d4ce849223edc08a
alexandrpi <alexandr.pipi@gmail.com>
2017-01-29 20:53:39 +0300

    operation-add-method
    Починил запросы к базе. Теперь есть рабочий метод добавления
    доходно-расходной операции в базу.

--- financial_db_worker/db_worker.py
+++ financial_db_worker/db_worker.py
@@ -1,18 +1,20 @@
-import pgdb
+import pg
+import datetime
 
 
 class FDBWorker:
     """Класс-подключение для работы с базой данных FinancialStatements"""
 
     def __init__(self, host='localhost', port=5432, username='postgres', password='postgres'):
-        try:
-            self.__connection = pgdb.connect(user=username,
-                                             password=password,
-                                             host='{host}:{port}'.format(host=host,
-                                                                         port=port),
-                                             database='FinancialStatements')
-        except:
-            self.__connection = None
+        self.__connection = pg.DB(user=username,
+                                  passwd=password,
+                                  host=host,
+                                  port=port,
+                                  dbname='FinancialStatements')
+        self.query = self.__connection.query
+
+    def create_schema(self, name):
+        pass
 
 
 class TableWorker:
@@ -22,6 +24,16 @@ class TableWorker:
         self.__db = db_connection
         self.__schema = schema
         self.__table = table
+        columns_query = 'SELECT column_name FROM information_schema.columns WHERE table_name=\'{table}\''
+        columns = self.__db.query(columns_query.format(table=self.__table)).getresult()
+        self.__columns = ['\"{}\"'.format(c[0]) for c in columns[1:]]
+
+    def insert(self, params):
+        template = 'INSERT INTO {schema}."{table}" ({names}) VALUES ({values})'
+        self.__db.query(template.format(schema=self.__schema,
+                                        names=', '.join(self.__columns),
+                                        table=self.__table,
+                                        values=', '.join(map(str, params))))
 
 
 class OperationsWorker(TableWorker):
@@ -30,5 +42,5 @@ class OperationsWorker(TableWorker):
     def __init__(self, db_connection, schema):
         super().__init__(db_connection, 'Operations', schema)
 
-    def add_operation(self):
-        pass
+    def add_operation(self, total, category_id, comment='', date=datetime.datetime.now()):
+        self.insert([category_id, total, '\'{}\''.format(date), '\'{}\''.format(comment)])
