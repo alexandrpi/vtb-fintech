4038aec07371f52decdb718f3f36c02d7851a218
alexandrpi <alexandr.pipi@gmail.com>
2017-01-29 22:55:26 +0300

    get-operations-method
    Теперь есть гибкий метод для получения выборки доходно-расходных
    операций.

--- financial_db_worker/db_worker.py
+++ financial_db_worker/db_worker.py
@@ -26,21 +26,43 @@ class TableWorker:
         self.__table = table
         columns_query = 'SELECT column_name FROM information_schema.columns WHERE table_name=\'{table}\''
         columns = self.__db.query(columns_query.format(table=self.__table)).getresult()
-        self.__columns = ['\"{}\"'.format(c[0]) for c in columns[1:]]
+        self._columns = ['\"{}\"'.format(c[0]) for c in columns[1:]]
 
-    def insert(self, params):
+    def _insert(self, params):
         template = 'INSERT INTO {schema}."{table}" ({names}) VALUES ({values})'
         self.__db.query(template.format(schema=self.__schema,
-                                        names=', '.join(self.__columns),
+                                        names=', '.join(self._columns),
                                         table=self.__table,
                                         values=', '.join(map(str, params))))
+
+    def _get(self, conditions, columns):
+        condition = ''
+        if len(conditions) > 0:
+            condition = 'WHERE {}'.format(' AND '.join(conditions))
+        template = 'SELECT {columns} FROM {schema}."{table}" {condition}'
+        result = self.__db.query(template.format(schema=self.__schema,
+                                                 table=self.__table,
+                                                 columns='*' if not len(columns) else ', '.join(columns),
+                                                 condition=condition))
+        return result.dictresult()
 
 
 class OperationsWorker(TableWorker):
     """Класс для работы с таблицей Operations"""
+    __OPERATION_CONDS = {'start_date': '"OperationDate" > \'{}\'',
+                         'end_date': '"OperationDate" < \'{}\'',
+                         'total_start': '"OperationTotal" > {}',
+                         'total_end': '"OperationTotal" < {}'}
 
     def __init__(self, db_connection, schema):
         super().__init__(db_connection, 'Operations', schema)
 
     def add_operation(self, total, category_id, comment='', date=datetime.datetime.now()):
-        self.insert([category_id, total, '\'{}\''.format(date), '\'{}\''.format(comment)])
+        self._insert([category_id, total, '\'{}\''.format(date), '\'{}\''.format(comment)])
+
+    def get_operations(self, *columns, **conds):
+        fields = ['\"{}\"'.format(c) for c in columns if c in self._columns]
+        conditions = []
+        if conds:
+            conditions = [self.__OPERATION_CONDS[key].format(conds[key]) for key in conds]
+        return self._get(conditions, fields)
