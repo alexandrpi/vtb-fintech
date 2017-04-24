dee3e44223de8c16d7b1f08d8a31cd8dd2d15423
ye11owSub <casio00@mail.ru>
2017-04-24 15:18:42 +0300

    Отчеты
    Добавылены отчеты и исключения в функции с вводом дат и сумм.

--- bot.py
+++ bot.py
@@ -14,7 +14,12 @@ sours = categories.get_categories()
 parentButtons = set([x['Parent'] for x in sours])
 childButtons = [x['Name'] for x in sours]
 type_id = None
+cat_id = None
 conn.close()
+statisticButtons = {'расходы': -1, 'доходы': 1}
+
+
+# print(statisticButtons.keys())
 
 
 # print(dataCategories)
@@ -28,9 +33,61 @@ def first_keyboard(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
     markup.add('Занесение информации о финансовой операции', 'Проверить состояние счета', 'Финансовая отчетность')
     bot.send_message(message.chat.id,
-                     'Привет, ' + message.chat.first_name + '\n \n Выберите пункт менюв зависимости от того, что вы хотите получить или сделать',
+                     'Привет, ' + message.chat.first_name + '\n \nВыберите пункт менюв зависимости от того, что вы хотите получить или сделать',
                      reply_markup=markup)
 
+
+def regist(message):
+    conn = fdbw.FDBWorker()
+    conn.create_user(str(message.chat.id), int(message.text))
+    first_keyboard(message)
+
+
+def operation_send(message):
+    try:
+        conn = fdbw.FDBWorker()
+        global type_id
+        ops = fdbw.OperationsWorker(conn, message.chat.id)
+        ops.add_operation(total=message.text, category_id=type_id)
+        bot.send_message(message.chat.id,'Выполненно')
+        conn.close()
+    except:
+        bot.send_message(message.chat.id, 'Некорректный ввод суммы, попробуйтей снова')
+
+# z = [{'Name': 'Выплата по кредиту', 'CategoryTotal': 500.0}, {'Name': 'Получен кредит', 'CategoryTotal': 10000.0}]
+# x = [x['Name'] + ": " + str(x['CategoryTotal']) + '\n' for x in z]
+# print(x)
+# y = ''
+# for x in x:
+#     y += x
+# print(y)
+# print(z[0])
+# cat_id = 1
+# a = [x for x in statisticButtons if x == cat_id]
+# print(a)
+# print(cat_id)
+
+
+def financial_analysis(message):
+    try:
+        date = message.text.split(',')
+        conn = fdbw.FDBWorker()
+        analysisOperation = fdbw.OperationsWorker(conn, message.chat.id)
+        if len(date) == 1:
+            analysisResult = analysisOperation.get_by_cat_type(date=date[0], cat_type=cat_id)
+        else:
+            analysisResult = analysisOperation.get_by_cat_type(start_date=date[0], end_date=date[1], cat_type=cat_id)
+        analysisText = [x['Name'] + ": " + str(x['CategoryTotal']) + '\n' for x in analysisResult]
+        text = ''
+        # title = [x for x in statisticButtons if statisticButtons.values() == cat_id]
+        # print(title)
+        for x in analysisText:
+            text += x
+        # bot.send_message(message.chat.id, title[0].upper() + '\n' + text)
+        bot.send_message(message.chat.id, text)
+        conn.close()
+    except:
+        bot.send_message(message.chat.id, 'Некорректный ввод, попробуйтей снова')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -43,23 +100,8 @@ def send_welcome(message):
     conn.close()
 
 
-def regist(message):
-    conn = fdbw.FDBWorker()
-    conn.create_user(str(message.chat.id), int(message.text))
-    first_keyboard(message)
-
-
-def operation_send(message):
-    conn = fdbw.FDBWorker()
-    global type_id
-    ops = fdbw.OperationsWorker(conn, message.chat.id)
-    ops.add_operation(total=message.text, category_id=type_id)
-    conn.close()
-
-
 @bot.message_handler(content_types=["text"])
 def keyboard(message):
-
     if message.text == 'Главное меню':
         first_keyboard(message)
 
@@ -92,39 +134,64 @@ def keyboard(message):
         bot.send_message(message.chat.id, "На вашем счете: " + str(balance))
         conn.close()
 
+    if message.text == 'Финансовая отчетность':
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
+        markup.add(*[types.KeyboardButton(text=x) for x in statisticButtons], 'отчет о финансовых результатах',
+                   'Главное меню')
+        # markup.add(*[x for x in statisticButtons], 'отчет о финансовых результатах') можно писать и так. никакой разницы
+        bot.send_message(message.chat.id, 'Выберите тип операций', reply_markup=markup)
 
-# print('asdsad')
-# bot.send_message()
-##ИНТЕРЕЕЕЕЕЕСНО!
-# mark = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-# a = types.KeyboardButton(text='asdasd',request_location=True)
-# mark.add(a)
-# bot.send_message(chat_id,text='asdas', reply_markup=mark)
-#
-# elif message.text == 'Приобретение материалов и товаров':
-#     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
-#     markup.add(*[x for x in buttons.keys() if buttons.values() == message.chat.id])
-# elif message.text == 'Кредиты и другие долги':
-#     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
-#     markup.add(*[x for x in buttons.keys() if buttons.values() == message.chat.id])
-# elif message.text == 'Оплаты от дебиторов/покупателей':
-#     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
-#     markup.add(*[x for x in buttons.keys() if buttons.values() == message.chat.id])
-#     # f = open('m.ogg', 'rb')
-#     # bot.send_audio(message.chat.id, f, """this place isn't yours anymore""")
+    if message.text in statisticButtons.keys():
+        global cat_id
+        cat_id = statisticButtons[message.text]
+        infoText = bot.send_message(message.chat.id, 'Введите 2 даты через запятую, в формате гггг-мм-дд')
+        bot.register_next_step_handler(infoText, financial_analysis)
+
+    if message.text == 'отчет о финансовых результатах':
+        conn = fdbw.FDBWorker()
+        statistic = fdbw.AssetsWorker(conn, message.chat.id)
+        # print(statistic.get_balance())
+        statistic = [x['Name'] + ': ' + str(x['CurrentTotal']) + '\n' for x in statistic.get_balance()]
+        text = ''
+        for x in statistic:
+            text += x
+        bot.send_message(message.chat.id, text)
+        conn.close()
+
+        # print('asdsad')
+        # bot.send_message()
+        ##ИНТЕРЕЕЕЕЕЕСНО!
+        # mark = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        # a = types.KeyboardButton(text='asdasd',request_location=True)
+        # mark.add(a)
+        # bot.send_message(chat_id,text='asdas', reply_markup=mark)
+        #
+        # elif message.text == 'Приобретение материалов и товаров':
+        #     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
+        #     markup.add(*[x for x in buttons.keys() if buttons.values() == message.chat.id])
+        # elif message.text == 'Кредиты и другие долги':
+        #     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
+        #     markup.add(*[x for x in buttons.keys() if buttons.values() == message.chat.id])
+        # elif message.text == 'Оплаты от дебиторов/покупателей':
+        #     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
+        #     markup.add(*[x for x in buttons.keys() if buttons.values() == message.chat.id])
+        #     # f = open('m.ogg', 'rb')
+        #     # bot.send_audio(message.chat.id, f, """this place isn't yours anymore""")
 
 
-# @bot.message_handler(content_types=["text"])
-# def trak(message):
-#     # for file in os.listdir("music"):
-#     #     if file.split(".")[-1] == "ogg":
-#     #         f = open('music/' + file, 'rb')
-#     #         res = bot.send_voice(message.chat.id, f, None)
-#     # time.sleep(3)
-#
-#     # bot.send_message(message.chat.id, text="Однако ты пидор")
-#     print(message.text)
-#     print(message)
-#     # print(message.last_name)
+        # @bot.message_handler(content_types=["text"])
+        # def trak(message):
+        #     # for file in os.listdir("music"):
+        #     #     if file.split(".")[-1] == "ogg":
+        #     #         f = open('music/' + file, 'rb')
+        #     #         res = bot.send_voice(message.chat.id, f, None)
+        #     # time.sleep(3)
+        #
+        #     # bot.send_message(message.chat.id, text="Однако ты пидор")
+        #     print(message.text)
+        #     print(message)
+        #     # print(message.last_name)
+
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
